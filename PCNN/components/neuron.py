3dac0ea19ae9e12a6e600a3a0cf491d9e867cfef
Stephan-kashkarov <stephankashkarovofficial@gmail.com>
2019-03-12 17:36:01 +1100

    added better system for initialising weights

--- PCNN/components/neuron.py
+++ PCNN/components/neuron.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+
 from PCNN.components.row import Base, Dummy
+
 
 class Grapher:
     
@@ -18,7 +20,6 @@ class Grapher:
 
     def show(self):
         plt.cla()
-        print(self.x)
         for label, data in self.vals.items():
             if len(data) != self.x[-1]:
                 print(f"ugh, {label} wasnt collected correctly {self.x}, {data}")
@@ -42,31 +43,52 @@ class Neuron:
         self.i = kwargs.get('i')
         self.j = kwargs.get('j')
         # Scalar parameters
+        self.yx = kwargs['yf'] if 'yf' in keys else np.float16(0.1)
         self.af = kwargs['af'] if 'af' in keys else np.float16(0.2)
         self.vf = kwargs['vf'] if 'vf' in keys else np.float16(0.1)
         self.al = kwargs['al'] if 'al' in keys else np.float16(0.5)
         self.vl = kwargs['vl'] if 'vl' in keys else np.float16(0.2)
         self.at = kwargs['at'] if 'at' in keys else np.float16(0.2)
-        self.vt = kwargs['vt'] if 'vt' in keys else np.float16(6)
+        self.vt = kwargs['vt'] if 'vt' in keys else np.float16(10)
         self.bias = kwargs['bias'] if 'bias' in keys else np.float16(0.5)
-        self.iterations = kwargs['it'] if 'it' in keys else 30
+        self.iterations = kwargs['it'] if 'it' in keys else 40
 
         # Matrix parameters
         self.input_neurons = np.empty([3,3], dtype=np.int8)
-        self.linker_weights = np.empty([3,3], dtype=np.float16)
+        self.linker_weights = np.array(
+            [
+                10 * np.random.random_sample(3),
+                10 * np.random.random_sample(3),
+                10 * np.random.random_sample(3),
+            ],
+            dtype=np.float16
+        )
+        self.feeder_weights = np.array(
+            [
+                10 * np.random.random_sample(3),
+                10 * np.random.random_sample(3),
+                10 * np.random.random_sample(3),
+            ],
+            dtype=np.float16
+        )
         print(self.linker_weights)
-        self.feeder_weights = np.empty([3,3], dtype=np.float16)
         print(self.feeder_weights)
 
         # Matricies
-        self.stimulus = np.float16(0.8)
+        self.stimulus = 10 * self.prev_row.neuron_arr[self.i, self.j]
         self.feed = np.float16(0)
         self.link = np.float16(0)
         self.u_act = np.float16(0)
-        self.activation = np.float16(0)
+        self.activation_internal = np.float16(0)
         self.theta = np.float16(1)
         self.plot_bool = kwargs['plot'] if 'plot' in keys else False
-        self.plotter = Grapher("feed", "link", "u_act", "theta", "activation")
+        self.plotter = Grapher(
+            "feed", 
+            "link", 
+            "u_act", 
+            "theta", 
+            "activation",
+        )
 
     # Linking methods
     def populate(self):
@@ -77,6 +99,7 @@ class Neuron:
         for x in k:
             for y in l:
                 self.input_neurons[x, y] = self.prev_row.neuron_arr[y, x]
+        self.stimulus = self.prev_row.neuron_arr[i, j]
         self.input_neurons[i, j] = 0
 
     # Mathematical Method
@@ -89,29 +112,28 @@ class Neuron:
         weighted_link = np.sum(np.multiply(self.input_neurons,self.linker_weights))
         self.feed = (feed_decay * self.feed) + (self.vf * weighted_feed) + self.stimulus
         self.link = (link_decay * self.link) + (self.vl * weighted_link)
-        self.theta = (theta_decay*self.theta) + (self.vt * self.activation)
+        self.theta = (theta_decay*self.theta) + (self.vt * self.activation_internal)
         self.u_act = self.feed * (1 + (self.bias * self.link))
-        self.activation = 1 if self.u_act > self.theta else 0
+        self.activation_internal = 1 if self.u_act > self.theta else 0
         self.n += 1
+        activation = (1 / (1 + np.exp(self.yx * (self.u_act - self.theta))))
         if self.plot_bool:
             self.plotter.graph(
                 feed=self.feed,
                 link=self.link,
                 theta=self.theta,
                 u_act=self.u_act,
-                activation=self.activation,
+                activation=activation,
             )
-        return self.activation
+        return activation
 
 
     # Operational Method
     def pulse(self, row, n):
-        print("Hi!")
         if self.n >= n:
-            print(f"Result state reached {self.n}")
             if self.plot_bool:
                 self.plotter.show()
-            return self.activation
+            return self.activation_internal
         else:
             self.calculate()
             return self.pulse(row, n) # gives value of every calculation from self.n to n
@@ -119,14 +141,17 @@ class Neuron:
 class Dummy_row:
 
     def __init__(self):
-        self.neuron_arr = np.ones((3, 3), dtype=np.int8)
+        self.neuron_arr = np.array(
+            [
+                np.random.random_sample(3),
+                np.random.random_sample(3),
+                np.random.random_sample(3),
+            ],
+            dtype=np.float16
+        )
 
 def test_run():
     n = Neuron(plot=True, i=1, j=1)
     r = Dummy_row()
-    print(r.neuron_arr)
-    print(n.iterations)
-    print(n.n)
     n.pulse(r, n.iterations)
     print("done")
-
