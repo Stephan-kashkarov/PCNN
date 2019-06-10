7f71b909defed1610afb5b96b9843156691e0a79
Stephan-kashkarov <kashkarovwork@gmail.com>
2019-06-10 14:19:04 +1000

    PCNN done

--- lib/pcnn.py
+++ lib/pcnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 class PCNN:
     def __init__(self, **kwargs):
@@ -15,27 +16,31 @@ class PCNN:
 
         # Graphing
         self.history = {
-            'activation': [],
-            'feed': [],
-            'link': [],
-            'u_activation': [],
             'theta': [],
+            'u_activation': [],
+            'link': [],
+            'feed': [],
+            'activation': [],
         }
 
     def __call__(self, x, iterations=10):
+
+        assert isinstance(x, np.array)
+        assert x.shape == self.input_shape
+        
         for neuron in self.neurons:
             his = neuron(x)
+            yield his[0]
             self.record(his)
         for i in range(iterations - 1):
             for neuron in self.neurons:
                 his = neuron(self.neurons.activation)
+                yield his[0]
                 self.record(his)
 
-    def record(self, **kwargs):
-        pass
-
-    def show(self, **kwargs):
-        pass
+    def record(self, his):
+        for key in self.history.keys():
+            self.history[key].append(his.pop())
 
 class Neuron:
     def __init__(self, **kwargs):
@@ -65,14 +70,15 @@ class Neuron:
         self.activation = np.float16(0)
 
         # Weight Tensors
-        self.linker_weights = np.random.rand(self.kernal_shape[0], self.kernal_shape[1])
-        self.feeder_weights = np.random.rand(self.kernal_shape[0], self.kernal_shape[1])
+        self.linker_weights = np.random.normal(size=(self.kernal_shape[0], self.kernal_shape[1]))
+        self.feeder_weights = np.random.normal(size=(self.kernal_shape[0], self.kernal_shape[1]))
 
         self.linker_weights[self.kernal_shape[1]/2, self.kernal_shape[0]/2] = 0
         self.feeder_weights[self.kernal_shape[1]/2, self.kernal_shape[0]/2] = 0
 
 
     def __call__(self, x):
+        
         layer_inputs = x[
             self.j - self.kernal_shape[1]/2:self.j + (self.kernal_shape[1]/2)+1,
             self.i - self.kernal_shape[0]/2:self.i + (self.kernal_shape[0]/2)+1,
@@ -80,9 +86,11 @@ class Neuron:
         
         feed_weights = self.vf * np.matmul(self.feeder_weights, layer_inputs)
         link_weights = self.vl * np.matmul(self.linker_weights, layer_inputs)
+
         self.feed = (np.exp(-self.af) * self.feed) + feed_weights
         self.link = (np.exp(-self.al) * self.link) + link_weights + x[self.j, self.i]
         self.u_activation = self.feed * (1 + (self.link) * self.bias)
         self.theta = (np.exp(-self.at)*self.theta) + self.vt * self.activation
         self.activation = 1 if self.u_activation > self.theta else 0
-        return [self.activation, (self.feed, self.link, self.u_activation, self.theta)]
+
+        return [self.activation, self.feed, self.link, self.u_activation, self.theta]
