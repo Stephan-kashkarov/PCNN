85e08d3b9d1e30d369fdc8de47756c871b1fd16e
Stephan-kashkarov <kashkarovwork@gmail.com>
2019-06-10 12:16:28 +1000

    started v2

---
+++ lib/model.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf
+
+class PCNN:
+    def __init__(self, **kwargs):
+        pass
+
+    def __call__(self, iterations=10):
+        pass
+
+class Neuron:
+    def __init__(self, **kwargs):
+        pass
+
+    def __call__(self):
+        pass
